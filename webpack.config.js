d4489ec2981c2392f7c3f55dc507d40b1508f859
Nirav <niravlimbachia031@gmail.com>
2024-06-11 09:59:36 +0530

    complate set up

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Nirav nayi',
+      template:"./public/index.html",
+      filename:"index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "./dist/main.js",
+  },
+  mode :"development",
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader,"css-loader", "postcss-loader"],
+      }
+    ]
+  }
+};
