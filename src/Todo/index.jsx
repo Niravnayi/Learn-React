1397137ea30ec075b47ab223d4edd87db97cca7c
Nirav <niravlimbachia031@gmail.com>
2024-06-21 09:47:20 +0530

    changes in todo

--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -1,138 +1,84 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
+    state = {
+        todoList: [],
+        todoText: "",
+    };
+
+    changeText = (event) => {
+        this.setState({ todoText: event.target.value })
+    };
+
+    addTodo = (event) => {
+        event.preventDefault();
+        this.setState(({ todoText, todoList }) => {
+            return {
+                todoList: [...todoList, { todoText, isDone: false, id: new Date().valueOf() }],
+                todoText: "",
+            }
+        })
+    }
+
+    updateTodo = (item) => {
+        this.setState(({ todoList }) => {
+            const index = todoList.findIndex((x) => x.id === item.id);
+            return {
+                todoList: [...todoList.slice(0, index), { ...todoList[index], isDone: !todoList[index].isDone }, ...todoList.slice(index + 1)]
+            }
+        })
+    }
+
+    deleteTodo = (item) => {
+        this.setState(({ todoList }) => {
+            const index = todoList.findIndex((x) => x.id === item.id)
+
+            return {
+                todoList: [
+                    ...todoList.slice(0, index), ...todoList.slice(index + 1)
+                ]
+            }
+        })
+    }
+
+
     render() {
+        const { todoList, todoText } = this.state;
         return (
             <div className="flex flex-col items-center h-screen overflow-hidden">
                 <h1 className="text-4xl font-semibold my-6">Todo App</h1>
                 {/* form */}
-                <form className="flex">
+                <form className="flex" onSubmit={this.addTodo}>
                     <div>
                         <label htmlFor="todoText" className="sr-only">Enter your todo</label>
-                        <input className="rounded-l-md" type="text" id="todoText" placeholder="Enter your todo here..." />
+                        <input className="rounded-l-md" type="text" id="todoText" placeholder="Enter your todo here..."
+                            value={todoText}
+                            onChange={this.changeText}
+                            required
+                        />
                     </div>
                     <button type="submit" className="btn rounded-l-none"
                     >Add Todo</button>
                 </form>
                 {/* list */}
                 <div className="flex flex-1 flex-col gap-4 w-full p-6 overflow-auto  ">
-                    {/* item 1 */}
-                <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, aperiam?</p>
-                    <button className="btn">Delete</button>
-                </div>
-                    {/* item 2 */}
-                <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, aperiam?</p>
-                    <button className="btn">Delete</button>
-                </div>
-                    {/* item 3 */}
-                <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 4 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 5 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 6 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 7 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 8 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 9 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 10 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 11 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 12 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
-                 {/* item 13 */}
-                 <div className="flex items-center">
-                    <div>
-                        <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
-                        <input type="checkbox" name="isCompleted" id="isCompleted" />
-                    </div>
-                    <p className="px-6 flex-1 line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et nobis esse voluptate voluptas illo vitae expedita vero ratione ea ad est aut, quibusdam blanditiis quos quia fuga dolore in debitis.</p>
-                    <button className="btn">Delete</button>
-                </div>
+                    {todoList.map((item, index) => {
+                        return (
+                            <div className="flex items-center" key={item.id}>
+                                <div>
+                                    <label htmlFor="isCompleted" className="sr-only">Toggle Complete</label>
+                                    <input type="checkbox" name="isCompleted" id="isCompleted" checked={item.isDone}
+                                        onChange={() => this.updateTodo(item)}
+                                    />
+                                </div>
+                                <p className="px-6 flex-1 line-clamp-2">{item.todoText}</p>
+                                <button className="btn"
+                                    onClick={() => this.deleteTodo(item)}
+                                >
+                                    Delete</button>
+                            </div>
+                        )
+                    })}
 
 
                 </div>
@@ -143,7 +89,7 @@ export default class Todo extends Component {
                     <button type="button" className="btn rounded-none flex-1">Complated </button>
 
                 </div>
-            </div>  
+            </div>
         )
     }
 }
