646c5f3995af0c0c84e9a492f04f3d1d9df1ab4e
Nirav <niravlimbachia031@gmail.com>
2024-06-28 22:23:49 +0530

    Start LifeCycle method

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,44 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
 import "./style.css";
-import Todo from "./Todo";
-import Weather from "./wether";
+import LifeCyle from "./LifeCycle";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById("app"));
 
+const test = "hello";
 
+LifeCyle.getDerivedStateFromProps = (props, state) => {
+  console.log("getDerivedStateFromProps Life Cycle");
+  return {
+    name: `Mr. ${props.name}`,
+    test,
+  };
+};
 
+export default class App extends Component {
+  state = {
+    name: "rohit",
+  };
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <LifeCyle name={name} />
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ name: "Virat" });
+          }}
+        >
+          Change Name
+        </button>
+      </div>
+    );
+  }
+}
 
-root.render(
-
-    // <Todo />
-    <Weather/>
-
-);
+root.render(<App />);
