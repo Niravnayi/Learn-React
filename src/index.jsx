51286c79358819724612244d2a46864a4bb33f41
Nirav <niravlimbachia031@gmail.com>
2024-07-10 16:46:27 +0530

    start todo

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import { Slider } from "./components/ui/slider";
+import Todo from "./Todo";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
@@ -11,18 +9,5 @@ document.body.innerHTML = '<div id="app"></div>';
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
 
-const App =() => {
-  return(
-    <div>
-      <Input type="email" placeholder="Email" />
-  <Button variant="destructive">Hello world</Button>
-  <Slider
-      defaultValue={[50]}
-      max={100}
-      step={1}
-      
-    />
-  </div>
-);
-};
-root.render(<App />);
+
+root.render(<Todo />);
