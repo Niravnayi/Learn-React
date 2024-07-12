11e5dd0c7b12cb654812c71a69befe2ad97bdf7c
Nirav <niravlimbachia031@gmail.com>
2024-07-12 11:49:23 +0530

    add new comoponents ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
 import Todo from "./Todo";
+import "./style.css";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
-
 
 root.render(<Todo />);
